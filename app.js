ec4b62be69fb4d70afabb872bf44c27fbc107f68
meet112114 <118681492+meet112114@users.noreply.github.com>
2025-02-20 18:33:51 +0530

    a

--- app.js
+++ app.js
@@ -7,11 +7,29 @@ dotenv.config({path:"./config.env"});
 const path = require('path');
 
 const app = express();
-app.use(cors({
-    origin: '*',  // Allow all origins (You can restrict it to your frontend IP)
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+
+const allowedOrigins = [
+  'http://localhost:3000', 
+  'https://wemprod-b.onrender.com',
+  'https://wemprod-f.onrender.com'
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Allow requests from allowed origins or no origin (like Postman)
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true); // Allow the request
+    } else {
+      callback(new Error('Not allowed by CORS')); // Reject the request
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true // This must be true to allow cookies and credentials
+};
+
+app.use(cors(corsOptions));
+
 
 app.use(session({
     resave: false,
