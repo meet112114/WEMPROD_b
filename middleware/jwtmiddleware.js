32ecb8c13877566dbcb120eac667a68fce353e41
meet112114 <118681492+meet112114@users.noreply.github.com>
2025-03-16 13:41:59 +0530

    Update jwtmiddleware.js

--- middleware/jwtmiddleware.js
+++ middleware/jwtmiddleware.js
@@ -3,15 +3,12 @@ const User = require('../models/user');
 
 const LoginAuth = async (req, res, next) => {
   try {
-    const authHeader = req.headers['authorization']; // Get the Authorization header
-    if (!authHeader) {
-      return res.status(401).json({ error: 'Unauthorized: No authorization header' });
-    }
+      const token = req.cookies.jwtoken || req.headers.authorization?.split(" ")[1]; 
 
-    const token = authHeader.split(' ')[1]; // Extract the token from the 'Bearer <token>' format
     if (!token) {
-      return res.status(401).json({ error: 'Unauthorized: No token provided' });
+      return res.status(401).send('Unauthorized: No token provided');
     }
+   
    
     const decodedToken = jwt.verify(token , process.env.SECRET_KEY);
     const rootUser = await User.findOne({ email : decodedToken.email });
